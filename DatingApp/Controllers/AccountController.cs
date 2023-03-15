fe67fd6a91a79f5c9546abe49b7721e1cd5370e9
Barış KAÇİN <playerone1923@gmail.com>
2023-03-15 22:50:33 +0300

    added login

--- DatingApp/Controllers/AccountController.cs
+++ DatingApp/Controllers/AccountController.cs
@@ -18,7 +18,6 @@ namespace DatingApp.Controllers
         }
 
         [HttpPost("register")] // POST: api/account/register
-
         public async Task<ActionResult<AppUser>>Register(RegisterDto registerDto)
         {
             if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
@@ -34,6 +33,25 @@ namespace DatingApp.Controllers
 
             context.Users.Add(user);
             await context.SaveChangesAsync();
+
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("Invalid User Name");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
 
             return user;
         }
